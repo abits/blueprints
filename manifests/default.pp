5d8870b104e4ea83493d727a2fb8a98b0f6ec8e0
Chris Martel <chris@codeways.org>
2013-04-28 22:51:02 +0200

    Work in progress.

--- manifests/default.pp
+++ manifests/default.pp
@@ -8,12 +8,20 @@ file { "/srv/www/drupal":
 class {'apache': }
 class {'apache::mod::php': }
 apache::vhost { 'www.d8.local':
+    require         => File['/srv/www/drupal'],
     priority        => '10',
     vhost_name      => '*',
     port            => '80',
     docroot         => '/srv/www/drupal',
     serveradmin     => 'admin@localhost',
     serveraliases   => ['drupal8.local',],
+}
+apache::mod { 'rewrite': }
+
+package { [php5-mysql, php5-gd, php5-intl, php-pear, php5-xdebug, sudo]:
+  ensure  => present,
+  require => Class['apache::mod::php'],
+  notify  => Service['httpd'],
 }
 
 class { 'mysql': }
@@ -25,4 +33,14 @@ mysql::db { 'drupal':
   password => 'drupal',
   host     => 'localhost',
   grant    => ['all'],
+}
+
+file { "/etc/php5/conf.d/xxx-custom.ini":
+  ensure => present,
+  owner => root,
+  group => root,
+  mode => 644,
+  source => ["puppet:///vagrant/files/php.ini"],
+  notify => Service["httpd"],
+  require => Class['apache::mod::php']
 }
