a8d40861ec5af3060373fc4ebe171de7e672e544
Chris Martel <chris@codeways.org>
2013-04-29 23:39:03 +0200

    Added phpmyadmin, composer, and drush.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,7 +1,7 @@
-file { "/srv/www/drupal":
-    ensure => "directory",
-    owner  => "root",
-    group  => "www-data",
+file { '/srv/www/drupal':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'www-data',
     mode   => 755,
 }
 
@@ -18,9 +18,19 @@ apache::vhost { 'www.d8.local':
 }
 apache::mod { 'rewrite': }
 
-package { [php5-mysql, php5-gd, php5-intl, php-pear, php5-xdebug, sudo]:
+package { [php5-mysql, 
+           php5-gd, 
+           php5-intl, 
+           php-pear, 
+           php5-xdebug, 
+           php5-cli,
+           sudo, 
+           phpmyadmin,
+           curl,
+           git]:
   ensure  => present,
-  require => Class['apache::mod::php'],
+  require => [ Exec['apt_update'], 
+               Class['apache::mod::php'] ],
   notify  => Service['httpd'],
 }
 
@@ -35,12 +45,70 @@ mysql::db { 'drupal':
   grant    => ['all'],
 }
 
-file { "/etc/php5/conf.d/xxx-custom.ini":
+file { '/etc/php5/conf.d/xxx-custom.ini':
   ensure => present,
   owner => root,
   group => root,
   mode => 644,
-  source => ["/vagrant/files/php.ini"],
-  notify => Service["httpd"],
+  source => ['/vagrant/files/php.ini'],
+  notify => Service['httpd'],
   require => Class['apache::mod::php']
+}
+
+exec { 'apt_update':
+  command => '/usr/bin/apt-get update'
+}
+
+exec { 'download_composer':
+  command => '/usr/bin/curl -s http://getcomposer.org/installer | php',
+  cwd => '/tmp',
+  require => Package['curl', 'php5-cli'],
+  creates => '/tmp/composer.phar',
+}
+file { '/usr/local/bin':
+  ensure => directory,
+}
+file { '/usr/local/bin/composer':
+  ensure => present,
+  source => '/tmp/composer.phar',
+  require => [ Exec['download_composer'], File['/usr/local/bin'],],
+  group => 'root',
+  mode => '0755',
+}
+exec { 'update_composer':
+  command => '/usr/local/bin/composer self-update',
+  require => File['/usr/local/bin/composer'],
+}
+
+file { 'phpmyadmin_config':
+  path => '/etc/phpmyadmin/config.inc.php',
+  source => '/vagrant/files/config.inc.php',
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0444',
+  require => Package[phpmyadmin],
+}
+
+exec { "pear auto_discover" :
+  command => "/usr/bin/pear config-set auto_discover 1",
+  require => [Package['php-pear']]
+}
+exec { "pear update-channels" :
+  command => "/usr/bin/pear update-channels",
+  require => [Package['php-pear'], Exec['pear auto_discover']]
+}
+exec {"pear install phpunit":
+  command => "/usr/bin/pear install --alldeps -s pear.phpunit.de/PHPUnit",
+  creates => '/usr/bin/phpunit',
+  require => Exec['pear update-channels']
+}
+exec {"pear install drush":
+  command => "/usr/bin/pear install --alldeps -s pear.drush.org/drush",
+  creates => '/usr/bin/drush',
+  require => Exec['pear update-channels']
+}
+exec {"pear install Console_Table":
+  command => "/usr/bin/pear install --alldeps -s --force Console_Table",
+  require => Exec['pear update-channels']
 }
